312cce6bfff6b5ae05fd2ae4b5d4ac1399f594ef
Vignesh Natarajan <vigneshnataraj@gmail.com>
2017-04-10 11:15:50 +1000

    add menu

--- test/controllers/menu_controller_test.rb
+++ test/controllers/menu_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class MenuControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get menu_index_url
+    assert_response :success
+    assert_select 'h1', 'Menu'
+    assert_select 'h2', 'Drinks'
+    assert_select 'h2', 'Food'
+  end
+
 end
